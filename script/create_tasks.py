c86199d80fcf906199586f0464955d5498a35a73
Andrea Stocco <astocco11@gmail.com>
2025-09-15 16:29:11 +0200

    update gitignore

--- script/create_tasks.py
+++ script/create_tasks.py
@@ -1,11 +1,20 @@
 from os import listdir
 
 files = [file for file in listdir('../DeletefreeSAS/')]
-algs = ['0', '1', '2', '3', '4', '5', '6']
-seeds = range(42, 52)
+algs = ['7', '8']
+# seeds = range(42, 52)
+intervals = [(0, 0.3), (0.3, 0.7), (0.7, 1)]
+intervals2 = [(0, 0.1), (0.1, 0.2), (0.2, 0.3), (0.3, 0.4), (0.4, 0.5), (0.5, 0.6), (0.6, 0.7), (0.7, 0.8), (0.8, 0.9), (0.9, 1)]
 
 with open('tasks.txt', 'w') as out:
     for file in files:
         for alg in algs:
-            for seed in seeds:
-                out.write('{} {} {}\n'.format(file, alg, seed))
+            #for seed in seeds:
+            for interval in intervals:
+                out.write('{} {} {} {} {}\n'.format(file, alg, 47, interval[0], interval[1]))
+                # out.write('{} {} {}\n'.format(file, alg, seed))
+
+with open('tasks.txt', 'a') as out:
+    for file in files:
+        for interval in intervals2:
+            out.write('{} {} {} {} {}\n'.format(file, 8, 47, interval[0], interval[1]))
